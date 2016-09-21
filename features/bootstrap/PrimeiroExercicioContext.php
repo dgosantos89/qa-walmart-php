defae410a04d8c731695996255051049650a6901
Diego Santos <dgo.santos89@gmail.com>
2016-09-21 12:03:49 -0300

    removed unused classes

--- features/bootstrap/PrimeiroExercicioContext.php
+++ features/bootstrap/PrimeiroExercicioContext.php
@@ -1,12 +1,8 @@
 <?php
 
-use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\MinkExtension\Context\MinkContext;
 use Behat\Behat\Context\SnippetAcceptingContext;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
-use GuzzleHttp\Client;
 
 /**
  * Defines application features from the specific context.
